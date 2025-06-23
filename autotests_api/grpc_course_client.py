4cf56d207c6e2324063d427728e063db204622e7
Anychnyi_Anton <anychnyianton@gmail.com>
2025-06-23 04:27:56 +0400

    commit_1

---
+++ autotests_api/grpc_course_client.py
@@ -0,0 +1,13 @@
+import grpc
+import course_service_pb2
+import course_service_pb2_grpc
+
+# Устанавливаем соединение с сервером
+channel = grpc.insecure_channel('localhost:50051')
+stub = course_service_pb2_grpc.CourseServiceStub(channel)
+# Отправляем запрос
+response = stub.GetCourse(course_service_pb2.GetCourseRequest(course_id="api-course"))
+print("Получен ответ от сервера:")
+print(f"ID курса: {response.course_id}")
+print(f"Название: {response.title}")
+print(f"Описание: {response.description}")
